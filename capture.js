9c9bc0be97c0c7307d02a72b35824163c698a8e1
phvs58027580-ux <phvs58027580@gmail.com>
2026-09-07 15:07:49 -0300

    Delete capture.js

--- capture.js
+++
@@ -1,1 +0,0 @@
-export class Capture{attempt(mon,bonus=0){const hp=1-mon.hp/mon.maxHp;const back=mon._backBonus||0;const chance=Math.min(.94,.18+hp*.62+bonus+back);return{success:Math.random()<chance,chance}}}
